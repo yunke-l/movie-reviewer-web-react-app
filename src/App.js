1e3db62362eaacb44eee7f09b368a215d379129d
Shaoyujie Chen <chenshaoyujie@ShaoyujieChens-MacBook-Air.local>
2023-08-15 21:37:51 -0700

    initilize setting, downloads libraries

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { HashRouter } from 'react-router-dom';
+import {Routes, Route, Navigate} from "react-router";
+import React from 'react';
+
+import FAcheck from "react-icons/fa";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaReact } from 'react-icons/fa';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <div className="container">
+        <h1>hello</h1>
+
+        < AiOutlineSearch />
+      </div>
+    </HashRouter>
   );
 }
 
