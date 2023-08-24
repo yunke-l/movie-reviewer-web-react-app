d766148cfa82b71574f27b544946cf635b476df9
Cassie Song <cassiesong@haidegongyuanxiaogezideMacBook-Air.local>
2023-08-24 10:54:21 -0700

    search page no result

--- src/movie-reviewer/search/search-thunks.js
+++ src/movie-reviewer/search/search-thunks.js
@@ -1,10 +1,29 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { findMovieBySearchTerm, findMovieByImdbID } from "./search-service";
 
+// export const findMovieBySearchTermThunk = createAsyncThunk(
+//     'findMovieBySearchTerm',
+//     (term) => findMovieBySearchTerm(term)
+// )
+
 export const findMovieBySearchTermThunk = createAsyncThunk(
     'findMovieBySearchTerm',
-    (term) => findMovieBySearchTerm(term)
-)
+    async (term) => {
+
+        const results = await findMovieBySearchTerm(term);
+        console.log(results);
+        if (!results) {
+            console.log("error");
+            throw new Error("No movies found for the given search term");
+            return false;
+        }
+        else {
+            return results;
+
+        }
+    }
+);
+
 export const findMovieByImdbIDThunk = createAsyncThunk(
     'findMovieByImdbID',
     (imdbID) => findMovieByImdbID(imdbID)
